9521d1593cf2f56f9b4b38c45da554d6e5720d3f
Rick445 <124686536+Rick445@users.noreply.github.com>
2023-06-19 18:38:47 +0200

    Merge branch 'Rick' of /Users/rick/Documents/GitHub/Frelder with conflicts.

--- app/src/main/java/com/example/frelder/MainActivity.kt
+++ app/src/main/java/com/example/frelder/MainActivity.kt
@@ -2,16 +2,8 @@ package com.example.frelder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Switch
 import android.widget.TextView
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.io.OutputStreamWriter
-import java.net.HttpURLConnection
-import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val clMain = findViewById<ConstraintLayout>(R.id.clMain)
+        val clMain = findViewById<ConstraintLayout>(R.id.cMain)
+        val textview = findViewById<TextView>(R.id.txtTitle)
 
+        fun onButtonClick(view: View) {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        swLamp?.setOnCheckedChangeListener({ _ , isChecked ->
+            if(isChecked){
+                textview.setText("on")
+                 clMain.setBackgroundResource(R.color.red)
+
+            }else{
+                textview.setText("off")
+                clMain.setBackgroundResource(R.color.white)
+
+
+            }
+        })
+        textview.setText("hallo test")
     }
 }
